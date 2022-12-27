d2121401e480e7363471fab2b35fc4bf2e339d82
David Star Kim <ollehstar@gmail.com>
2022-12-27 23:59:25 +0900

    221227 - 오늘은 새벽 커밋 하게씀다!

---
+++ codility/StoneWall_221227.java
@@ -0,0 +1,13 @@
+package codility;
+
+public class StoneWall_221227 {
+    public static void main(String[] args) {
+        int [] H = "";
+
+        //12시 넘어가고 커밋하겠습니다!!!!
+    }
+    public static int solution(int[] H) {
+        // Implement your solution here
+        return 0;
+    }
+}
