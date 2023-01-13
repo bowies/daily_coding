3e070b51c92b6ea2da5ef6d1d9fad42ce16504e6
David Star Kim <ollehstar@gmail.com>
2023-01-13 21:39:16 +0900

    230113 - 오류 수정용 Commit (package 오류)

---
+++ codility/MinMaxDivision_230113.java
@@ -0,0 +1,13 @@
+package codility;
+
+public class MinMaxDivision_230113 {
+
+    public static void main(String[] args) {
+        
+    }
+
+    
+    public int solution(int K, int M, int[] A) {
+        // Implement your solution here
+    }
+}
