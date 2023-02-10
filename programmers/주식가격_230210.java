78659a242d8d1bf5be337fe9153ab9ecc2ce3a82
David Star Kim <ollehstar@gmail.com>
2023-02-10 23:58:43 +0900

    230210 - 푸는중!

---
+++ programmers/주식가격_230210.java
@@ -0,0 +1,15 @@
+package programmers;
+
+public class 주식가격_230210 {
+    public static void main(String[] args) {
+        int [] prices = {1, 2, 3, 2, 3};
+        System.out.println(solution(prices));
+    }
+
+
+    public static int[] solution(int[] prices) {
+        int[] answer = {};
+
+        return answer;
+    }
+}
