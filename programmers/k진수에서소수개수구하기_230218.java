c9dd2f535efa454e6d6e5d3667b74f9f32a1c3e9
David Star Kim <ollehstar@gmail.com>
2023-02-18 23:57:34 +0900

    230218 - 푸는중

---
+++ programmers/k진수에서소수개수구하기_230218.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class k진수에서소수개수구하기_230218 {
+    public static void main(String[] args) {
+
+    }
+
+    public int solution(int n, int k) {
+        int answer = -1;
+        return answer;
+    }
+}
