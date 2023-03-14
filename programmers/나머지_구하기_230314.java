c8d401df4db2db09e4b673a83a39e5dc86cbd837
David Star Kim <ollehstar@gmail.com>
2023-03-14 23:54:23 +0900

    230315 - Daily Commit!

---
+++ programmers/나머지_구하기_230314.java
@@ -0,0 +1,7 @@
+package programmers;
+
+class 나머지_구하기_230314 {
+    public int solution(int num1, int num2) {
+        return num1%num2;
+    }
+}
---
+++ programmers/두_수의_곱_230313.java
@@ -0,0 +1,8 @@
+package programmers;
+
+public class 두_수의_곱_230313 {
+        public int solution(int num1, int num2) {
+
+        return num1*num2;
+    }
+}
