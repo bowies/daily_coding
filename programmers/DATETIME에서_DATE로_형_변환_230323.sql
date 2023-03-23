deddc8d0a22fb9dd66642d6be8d21aa2a0d194ef
David Star Kim <ollehstar@gmail.com>
2023-03-23 21:34:51 +0900

    230323 - Daily Commit! (1/2)

---
+++ programmers/DATETIME에서_DATE로_형_변환_230323.sql
@@ -0,0 +1,17 @@
+--MySQL
+SELECT
+    ANIMAL_ID,
+    NAME,
+    DATE_FORMAT(DATETIME, '%Y-%m-%d') AS 날짜
+FROM
+    ANIMAL_INS
+
+--Oracle
+SELECT
+    ANIMAL_ID,
+    NAME,
+    TO_CHAR(DATETIME, 'YYYY-MM-DD') AS 날짜
+FROM
+    ANIMAL_INS
+ORDER BY
+    ANIMAL_ID
