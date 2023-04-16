f5ebe94a57f56be98da5acf4d21c5d55c2c38c29
David Star Kim <ollehstar@gmail.com>
2023-04-16 22:59:14 +0900

    230416 - Daily Commit! (1/2) 1일1팩 2일차

---
+++ programmers/흉부외과_또는_일반외과_의사_목록_출력하기_230416.sql
@@ -0,0 +1,45 @@
+--Mysql
+SELECT
+    DR_NAME,
+    DR_ID,
+    MCDP_CD,
+    DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD
+FROM
+    DOCTOR
+WHERE
+        MCDP_CD = 'CS'
+   OR
+        MCDP_CD = 'GS'
+ORDER BY
+    HIRE_YMD DESC,
+    DR_NAME ASC;
+
+--Oracle
+SELECT
+    DR_NAME,
+    DR_ID,
+    MCDP_CD,
+    TO_CHAR(HIRE_YMD, 'YYYY-MM-DD') AS HIRE_YMD
+FROM
+    DOCTOR
+WHERE
+        MCDP_CD = 'CS'
+   OR
+        MCDP_CD = 'GS'
+ORDER BY
+    HIRE_YMD DESC,
+    DR_NAME ASC;
+
+--Oracle
+SELECT
+    DR_NAME,
+    DR_ID,
+    MCDP_CD,
+    TO_CHAR(HIRE_YMD, 'YYYY-MM-DD') AS HIRE_YMD
+FROM
+    DOCTOR
+WHERE
+        MCDP_CD IN ('CS', 'GS')
+ORDER BY
+    HIRE_YMD DESC,
+    DR_NAME ASC
