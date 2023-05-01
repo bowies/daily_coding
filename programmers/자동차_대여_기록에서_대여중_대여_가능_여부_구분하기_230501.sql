cd98af3e2b1c27b6606d24000bf67375a9a9d16d
David Star Kim <ollehstar@gmail.com>
2023-05-01 20:36:00 +0900

    230501 - Daily Commit! (1/2) 1일1팩 17일째 아직안함

---
+++ programmers/자동차_대여_기록에서_대여중_대여_가능_여부_구분하기_230501.sql
@@ -0,0 +1,47 @@
+--Mysql
+SELECT CAR_ID,
+       CASE
+           WHEN
+                   CAR_ID
+                   IN (
+                       SELECT
+                           CAR_ID
+                       FROM
+                           CAR_RENTAL_COMPANY_RENTAL_HISTORY
+                       WHERE
+                           '2022-10-16'
+                               BETWEEN
+                               START_DATE
+                               AND
+                               END_DATE
+                   )THEN '대여중'
+           ELSE '대여 가능'
+           END AS AVAILABILITY
+FROM
+    CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY
+    CAR_ID
+ORDER BY
+    CAR_ID DESC;
+
+--Oracle
+SELECT
+    CAR_ID,
+    MAX(
+            CASE
+                WHEN
+                    '2022-10-16'
+                        BETWEEN
+                        TO_CHAR(START_DATE,'YYYY-MM-DD')
+                        AND
+                        TO_CHAR(END_DATE,'YYYY-MM-DD')
+                    THEN '대여중'
+                ELSE
+                    '대여 가능'
+                END) AS AVAILABILITY
+FROM
+    CAR_RENTAL_COMPANY_RENTAL_HISTORY
+GROUP BY
+    CAR_ID
+ORDER BY
+    CAR_ID DESC;
