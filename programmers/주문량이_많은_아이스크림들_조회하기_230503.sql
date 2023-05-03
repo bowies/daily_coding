31f651a6dd7f93933c56b60698bbe483dc0dfb20
David Star Kim <ollehstar@gmail.com>
2023-05-03 23:37:22 +0900

    230503 - Daily Commit! (1/2) 1일1팩 18일

---
+++ programmers/주문량이_많은_아이스크림들_조회하기_230503.sql
@@ -0,0 +1,36 @@
+--Mysql
+SELECT
+    F.FLAVOR
+FROM
+    FIRST_HALF F
+        LEFT JOIN (
+        SELECT
+            FLAVOR,
+            SUM(TOTAL_ORDER) AS TOTAL_ORDER
+        FROM
+            JULY
+        GROUP BY
+            FLAVOR
+    ) J
+                  ON F.FLAVOR = J.FLAVOR
+ORDER BY
+    (F.TOTAL_ORDER + J.TOTAL_ORDER) DESC LIMIT 3;
+
+--Oracle
+SELECT
+    F.FLAVOR
+FROM(
+        SELECT
+            H.FLAVOR,
+            SUM(H.TOTAL_ORDER) + SUM(J.TOTAL_ORDER) AS TOTAL
+        FROM FIRST_HALF H
+                 INNER JOIN
+             JULY J
+             ON H.FLAVOR = J.FLAVOR
+        GROUP BY
+            H.FLAVOR
+        ORDER BY
+            TOTAL DESC
+    ) F
+WHERE
+        ROWNUM <= 3
