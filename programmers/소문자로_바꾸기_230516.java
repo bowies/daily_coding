77b66e836fa94567d534cf5f619f07aa2a8a5acb
David Star Kim <ollehstar@gmail.com>
2023-05-16 22:41:31 +0900

    230516 - Daily Commit! (1/2) 1일1팩 31일 (아직안함)

--- programmers/소문자로_바꾸기_230516.java
+++ programmers/소문자로_바꾸기_230516.java
@@ -3,6 +3,7 @@ package programmers;
 import java.util.Locale;
 
 class 소문자로_바꾸기_230516 {
+
     public String solution(String myString) {
         String answer = myString.toLowerCase(Locale.ENGLISH);
 
