72c0014a7778086852e974ae9a176130b6349f5b
David Kim <ollehstar@gmail.com>
2023-05-19 12:48:06 +0900

    230519 - Daily Commit!

---
+++ programmers/공배수_230519.java
@@ -0,0 +1,15 @@
+import java.util.stream.IntStream;
+
+package programmers;
+class Solution {
+    public int solution(int number, int n, int m) {
+        return (number % n == 0 && number % m == 0) ? 1 : 0;
+    }
+
+    public int solution2(int number, int n, int m) {
+        //굳이 Stream을 써서 이렇게도 할 수 있음 
+        return IntStream.of(number).filter(num -> num % n == 0 && num % m == 0).count() > 0 ? 1 : 0;
+    }
+}
+
+}
