af3dc1ff978b3e4cfc706773abedd132ae2842b1
David Star Kim <ollehstar@gmail.com>
2023-05-20 22:40:36 +0900

    230520 - Daily Commit! (1/2) 1일1팩 35일

---
+++ programmers/정수_부분_230520.java
@@ -0,0 +1,13 @@
+package programmers;
+
+class 정수_부분_230520 {
+    public int solution(double flo) {
+        int answer = (int) flo;
+        return answer;
+    }
+
+    public int solution2(double flo) {
+        int answer = (int) Math.floor(flo);
+        return answer;
+    }
+}
