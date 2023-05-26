472d8182f077e9857fb3e62b6b467808e4a33a4b
David Kim <ollehstar@gmail.com>
2023-05-26 16:47:56 +0900

    230526 - Daily Commit!

---
+++ programmers/배열_뒤집기_230526.java
@@ -0,0 +1,20 @@
+package programmers;
+
+import java.util.Arrays;
+import java.util.stream.IntStream; 
+
+class Solution {
+    public int[] solution_Intstream(int[] num_list) {
+        return IntStream.range(0, num_list.length)
+                .map(i -> num_list[num_list.length - 1 - i])
+                .toArray();
+    }
+    
+    public int[] solution_for(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        for(int i = num_list.length-1 ; i >= 0; i--){
+            answer[num_list.length-1-i] = num_list[i];
+        }
+        return answer;
+    }
+}
