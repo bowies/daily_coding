2782ba4109f952b78aabb19e1b3559a9440cce06
David Kim <ollehstar@gmail.com>
2023-06-02 10:51:18 +0900

    230602 - Daily Commit!

---
+++ programmers/중복된_숫자_개수_230602.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int 중복된_숫자_개수_230602(int[] array, int n) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int num : array) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        return countMap.getOrDefault(n, 0);
+    }
+}
