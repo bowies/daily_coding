980413c88624930b61d2ada2d85725b4cf14da33
David Star Kim <ollehstar@gmail.com>
2023-06-09 23:41:12 +0900

    230609 - Daily Commit! (1/2) 1일1팩 55일 (아직안함)

---
+++ programmers/n_번째_원소까지_230609.java
@@ -0,0 +1,24 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class n_번째_원소까지_230609 {
+
+    public int[] solution(int[] num_list, int n) {
+        //정수 리스트 num_list와 정수 n이 주어질 때,
+        // num_list의 첫 번째 원소부터 n 번째 원소까지의
+        // 모든 원소를 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+
+        int[] answer = new int[n];
+
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = num_list[i];
+        }
+
+        return answer;
+    }
+
+    public int[] solution_short(int[] num_list, int n) {
+        return Arrays.copyOfRange(num_list, 0, n);
+    }
+}
