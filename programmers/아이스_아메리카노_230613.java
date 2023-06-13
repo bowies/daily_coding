beaf2e829a700693dd926e4194db37ba1bc355bb
David Kim <ollehstar@gmail.com>
2023-06-13 18:44:23 +0900

    230613 - Daily Commit!

---
+++ programmers/아이스_아메리카노_230613.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        int coffee = 5500;
+        answer[0] = money/coffee;
+        answer[1] = money - answer[0] * coffee;
+        
+        
+        return answer;
+    }
+
+
+    
+    public int[] solution_oneline(int money) {
+        return new int[] { money / 5500, money % 5500 };
+    }
+}
