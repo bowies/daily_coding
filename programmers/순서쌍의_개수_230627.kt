8bbba39cb1d01d9b9649ce6b6a3a9af6946ec7b3
David Star Kim <ollehstar@gmail.com>
2023-06-27 22:07:56 +0900

    230627 - Daily Commit! (1/2) 1일1팩 73일

---
+++ programmers/순서쌍의_개수_230627.kt
@@ -0,0 +1,23 @@
+package programmers
+
+class 순서쌍의_개수_230627 {
+    // 순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 나타낸 쌍으로 (a, b)로 표기합니다.
+    // 자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 return하도록 solution 함수를 완성해주세요.
+
+
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+
+        for (i in 1..n) {
+            if (n % i == 0) {
+                answer++
+            }
+        }
+
+
+        return answer
+    }
+
+    fun solution_short(n: Int) = (1..n).count { n % it == 0 }
+
+}
