7ef597cefe9f939807572525a5834ec2fe99a06a
David Star Kim <ollehstar@gmail.com>
2023-07-25 23:48:02 +0900

    230725 - Daily Commit! 1일1팩 101일

---
+++ programmers/특수문자_출력하기_Java_230725.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class 특수문자_출력하기_Java_230725 {
+    //!@#$%^&*(\'"<>?:; 를 출력하시오
+    public static void main(String[] args) {
+        System.out.println("!@#$%^&*(\\'\"<>?:;");
+        //작은 따옴표(')와 큰 따옴표(")를 출력할 때 이스케이프 문자를 사용하여 백슬래시(')와 백슬래시(")를 함께 사용해야 정상 출력됨
+    }
+
+
+    public static void main2(String[] args) {
+        System.out.print(
+                "!"+"@"+"#"+"$"+"%"+"^"+"&"+"*"+"("+"\\"+"\'"+"\""+"<"+">"+"?"+":"+";");
+    } //ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+
+
+}
---
+++ programmers/특수문자_출력하기_Kotlin_230725.kt
@@ -0,0 +1,26 @@
+package programmers
+
+class 특수문자_출력하기_Kotlin_230725 {
+    fun main() {
+        println("!@#\$%^&*(\\'\"<>?:;")
+    }
+    fun main2(args: Array<String>) {
+        print(Char(33)) // !
+        print(Char(64)) // @
+        print(Char(35)) // #
+        print(Char(36)) // $
+        print(Char(37)) // %
+        print(Char(94)) // ^
+        print(Char(38)) // &
+        print(Char(42)) // *
+        print(Char(40)) // (
+        print(Char(92)) // \
+        print(Char(39)) // '
+        print(Char(34)) // "
+        print(Char(60)) // <
+        print(Char(62)) // >
+        print(Char(63)) // ?
+        print(Char(58)) // :
+        print(Char(59)) // ;
+    } //ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
+}
