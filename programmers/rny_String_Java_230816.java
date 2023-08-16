77f8cc355a0c8d00f24231994a77c2fe90a810a1
David Kim <ollehstar@gmail.com>
2023-08-16 15:23:27 +0900

    230816 - PR 요청 예정입니다.

---
+++ programmers/rny_String_Java_230816.java
@@ -0,0 +1,8 @@
+class rny_String_Java_230816 {
+    public String solution(String rny_string) {
+        String answer = rny_string.replaceAll("[m]", "rn");
+        return answer;
+    }
+
+
+}
---
+++ programmers/rny_String_Kotlin_230816.kt
@@ -0,0 +1,6 @@
+
+class rny_String_Kotlin_230816 {
+    fun solution(rny_string: String): String {
+        return rny_string.replace("m", "rn")
+    }
+}
