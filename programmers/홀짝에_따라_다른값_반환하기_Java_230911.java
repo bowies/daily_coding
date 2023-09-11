8a92ced105c920e2f67fcb5e5869ea6ff1248ec8
David Kim <ollehstar@gmail.com>
2023-09-11 20:08:29 +0900

    230911

--- programmers/홀짝에_따라_다른값_반환하기_Java_230911.java
+++ programmers/홀짝에_따라_다른값_반환하기_Java_230911.java
@@ -2,7 +2,7 @@ package programmers;
 
 import java.util.stream.IntStream;
 
-class Solution {
+class 홀짝에_따라_다른값_반환하기_Java_230911 {
     public int solution(int n) {
         return n % 2 == 0 ? 
             IntStream.rangeClosed(1, n).filter(i -> i % 2 == 0).map(i -> i * i).sum() :
