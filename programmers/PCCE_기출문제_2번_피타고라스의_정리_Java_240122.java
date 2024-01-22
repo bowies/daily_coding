cfac0aae0ad6271f0d7fb7af2b56adf604dcd1c3
David Kim <ollehstar@gmail.com>
2024-01-22 21:08:21 +0900

    240122

---
+++ programmers/PCCE_기출문제_2번_피타고라스의_정리_Java_240122.java
@@ -0,0 +1,15 @@
+package programmers
+
+import java.util.Scanner;
+
+class PCCE_기출문제_2번_피타고라스의_정리_Java_240122 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+
+        int b_square = c*c - a*a;
+
+        System.out.println(b_square);
+    }
+}
