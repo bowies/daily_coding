6f42edeb2c8fa4b7bc8bb49edfec9b4ad4f608ae
David Kim <ollehstar@gmail.com>
2024-01-30 21:27:08 +0900

    240130

---
+++ programmers/a와_b_출력하기_Java_240130.java
@@ -0,0 +1,12 @@
+package programmers;
+import java.util.Scanner;
+
+public class a와_b_출력하기_Java_240130 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a +  "\nb = " + b);
+    }
+}
