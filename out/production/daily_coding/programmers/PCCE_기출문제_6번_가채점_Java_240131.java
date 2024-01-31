f37a10057f82153c76088f627f6d9dd61a53e07f
bowies <ollehstar@gmail.com>
2024-01-31 13:42:22 +0900

    240131

---
+++ out/production/daily_coding/programmers/PCCE_기출문제_6번_가채점_Java_240131.java
@@ -0,0 +1,19 @@
+package programmers;
+
+class PCCE_기출문제_6번_가채점_Java_240131 {
+    public String[] solution(int[] numbers, int[] our_score, int[] score_list) {
+        int num_student = numbers.length;
+        String[] answer = new String[num_student];
+
+        for (int i = 0; i < num_student; i++) {
+            if (our_score[i] == score_list[numbers[i]-1]) {
+                answer[i] = "Same";
+            }
+            else {
+                answer[i] = "Different";
+            }
+        }
+
+        return answer;
+    }
+}
