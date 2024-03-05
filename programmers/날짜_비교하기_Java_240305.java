f29e6481d49dfc0e5ff99fa570705319c230c7a9
bowies <ollehstar@gmail.com>
2024-03-05 16:46:45 +0900

    240305

--- programmers/날짜_비교하기_Java_240305.java
+++ programmers/날짜_비교하기_Java_240305.java
@@ -11,4 +11,13 @@ class 날짜_비교하기_Java_240305 {
         }
         return 0;
     }
+    public int solution_2(int[] date1, int[] date2) {
+        for (int i = 0; i < 3; i++) {
+            int answer = Integer.compare(date1[i], date2[i]);
+            if (answer != 0) {
+                return answer < 0 ? 1 : 0;
+            }
+        }
+        return 0;
+    }
 }
