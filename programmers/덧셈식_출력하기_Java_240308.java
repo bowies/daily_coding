5723fd936aa10a537e5acb492b85880fc63b5b8f
bowies <ollehstar@gmail.com>
2024-03-08 21:32:38 +0900

    240308 - 10sec...

---
+++ programmers/덧셈식_출력하기_Java_240308.java
@@ -0,0 +1,13 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class 덧셈식_출력하기_Java_240308 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
