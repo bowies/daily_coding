8a03c7f04b907fb54de508f55a1892cad26109fa
STAR <ollehstar@gmail.com>
2024-03-14 23:42:50 +0900

    240314

---
+++ programmers/문자열_출력하기_Java_240314.java
@@ -0,0 +1,11 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class 문자열_출력하기_Java_240314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine(); 
+        System.out.println(str); 
+    }
+}
