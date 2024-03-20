1d55d0f43697fa041f71a9022639e5217c8effae
STAR <ollehstar@gmail.com>
2024-03-20 23:09:32 +0900

    240320

---
+++ programmers/간단한_논리_연산_Java_240320.java
@@ -0,0 +1,8 @@
+package programmers;
+
+class 간단한_논리_연산_Java_240320 {
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+        boolean result = (x1 || x2) && (x3 || x4);
+        return result;
+    }
+}
