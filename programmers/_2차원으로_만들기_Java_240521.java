ddc6de0222701830900dcef8e38ff38408fbcff0
STAR <ollehstar@gmail.com>
2024-05-22 23:40:49 +0900

    240522

--- programmers/_2차원으로_만들기_Java_240521.java
+++ programmers/_2차원으로_만들기_Java_240521.java
@@ -1,6 +1,6 @@
 package programmers;
 
-class Solution {
+class _2차원으로_만들기_Java_240521 {
     public int[][] solution(int[] num_list, int n) {
         int length = num_list.length;
         int[][] answer = new int[length / n][n];
