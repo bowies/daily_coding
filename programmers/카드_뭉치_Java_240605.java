9bce5cd987d8ced114e9fb3ff2d43c8e26e88a32
STAR <ollehstar@gmail.com>
2024-06-05 23:47:34 +0900

    240605

---
+++ programmers/카드_뭉치_Java_240605.java
@@ -0,0 +1,19 @@
+package programmers;
+
+class 카드_뭉치_Java_240605 {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        int i = 0, j = 0;
+        
+        for (String word : goal) {
+            if (i < cards1.length && word.equals(cards1[i])) {
+                i++;
+            } else if (j < cards2.length && word.equals(cards2[j])) {
+                j++;
+            } else {
+                return "No";
+            }
+        }
+        
+        return "Yes";
+    }
+}
