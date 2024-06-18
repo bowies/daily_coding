33429e54d62f507c07c9bbc71b816275d662f5e6
STAR <ollehstar@gmail.com>
2024-06-18 23:54:17 +0900

    240618

--- programmers/PCCE_기출문제_9번_이웃한_칸_Java_240618.java
+++ programmers/PCCE_기출문제_9번_이웃한_칸_Java_240618.java
@@ -2,22 +2,22 @@ package programmers;
 
 class PCCE_기출문제_9번_이웃한_칸_Java_240618 {
     public int solution(String[][] board, int h, int w) {
-        int n = board.length;
-        int count = 0;
-        int[] dh = {0, 1, -1, 0};
-        int[] dw = {1, 0, 0, -1};
+        int length = board.length;
+        int answer = 0;
+        int[] h방향 = {0, 1, -1, 0};
+        int[] w방향 = {1, 0, 0, -1};
 
         for (int i = 0; i < 4; i++) {
-            int h_check = h + dh[i];
-            int w_check = w + dw[i];
+            int h좌표 = h + h방향[i];
+            int w좌표 = w + w방향[i];
 
-            if (0 <= h_check && h_check < n && 0 <= w_check && w_check < n) {
-                if (board[h][w].equals(board[h_check][w_check])) {
-                    count++;
+            if (0 <= h좌표 && h좌표 < length && 0 <= w좌표 && w좌표 < length) {
+                if (board[h][w].equals(board[h좌표][w좌표])) {
+                    answer++;
                 }
             }
         }
 
-        return count;
+        return answer;
     }
 }
