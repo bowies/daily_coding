5a700df808e98f898524879e9f8ad48695d33533
bowies <ollehstar@gmail.com>
2024-07-03 20:03:15 +0900

    240703

---
+++ programmers/부서별_평균_연봉_조회하기_MySQL_240703.sql
@@ -0,0 +1,17 @@
+SELECT
+    HD.DEPT_ID,
+    HD.DEPT_NAME_EN,
+    --AVG(ROUND(HE.SAL, 0)) AS AVG_SAL -- 평균 구한 뒤에 반올림 하는 것이 맞는 계산
+    ROUND(AVG(HE.SAL), 0) AS AVG_SAL
+FROM
+    HR_DEPARTMENT HD
+    JOIN
+        HR_EMPLOYEES HE
+    ON
+        HD.DEPT_ID = HE.DEPT_ID
+GROUP BY
+    HD.DEPT_ID,
+    HD.DEPT_NAME_EN
+ORDER BY
+    AVG_SAL DESC;
+
