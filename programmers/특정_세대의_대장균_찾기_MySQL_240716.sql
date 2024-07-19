1398d713c57eb027764386ccb88aedfd4f9358ae
STAR <ollehstar@gmail.com>
2024-07-19 23:59:02 +0900

    240719

---
+++ programmers/특정_세대의_대장균_찾기_MySQL_240716.sql
@@ -0,0 +1,16 @@
+SELECT
+    ID
+FROM
+    ECOLI_DATA
+WHERE
+    PARENT_ID IN (
+        SELECT ID
+        FROM ECOLI_DATA
+        WHERE PARENT_ID IN (
+            SELECT ID
+            FROM ECOLI_DATA
+            WHERE PARENT_ID IS NULL
+        )
+    )
+ORDER BY
+    ID;
