a5060c5db74bcea6512e6d1597d5cd725e0216c0
STAR <ollehstar@gmail.com>
2024-07-22 00:14:52 +0900

    240722

---
+++ programmers/FrontEnd_개발자_찾기_MySQL_240722.sql
@@ -0,0 +1,17 @@
+SELECT
+    D.ID,
+    D.EMAIL,
+    D.FIRST_NAME,
+    D.LAST_NAME
+FROM
+    DEVELOPERS D
+    JOIN
+        SKILLCODES S
+            ON
+                (D.SKILL_CODE & S.CODE) = S.CODE
+WHERE
+    S.CATEGORY = 'Front EnD'
+GROUP BY
+    D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
+ORDER BY
+    D.ID;
