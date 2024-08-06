f659fae9f8bfa9a15bdf18c3f690628a2877ecef
bowies <ollehstar@gmail.com>
2024-08-06 22:00:49 +0900

    240806

--- programmers/약수의_개수와_덧셈_Java_240803.java
+++ programmers/약수의_개수와_덧셈_Java_240803.java
@@ -2,6 +2,26 @@ package programmers;
 
 class 약수의_개수와_덧셈_Java_240803 {
 
+
+    public int solution_good(int left, int right) {
+        int answer = 0;
+
+        for (int i=left;i<=right;i++) {
+            //제곱수인 경우 약수의 개수가 홀수
+            if (i % Math.sqrt(i) == 0) {
+                answer -= i;
+            }
+            //제곱수가 아닌 경우 약수의 개수가 짝수
+            else {
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+
+
+
     public int solution(int left, int right) {
         int answer = 0;
 
