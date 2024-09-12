bf73f3c5780bdc53213bb20ce7c1120de6004570
bowies <ollehstar@gmail.com>
2024-09-12 13:01:46 +0900

    240912

---
+++ programmers/PCCE_기출문제_1번_문자_출력_Java_240912.java
@@ -0,0 +1,11 @@
+package programmers;
+import java.util.Scanner;
+
+public class PCCE_기출문제_1번_문자_출력_Java_240912 {
+    public static void main(String[] args) {
+        String message = "        Let's go!        ";
+
+        System.out.println("3                \n                2                \n                1");
+                System.out.println(message);
+    }
+}
